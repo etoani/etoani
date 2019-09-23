14c104f3eb887947393d972c3bcf5dc6b6025bfb
etoani <54567774+etoani@users.noreply.github.com>
2019-09-22 21:14:30 -0700

    Checkpiont clean bootstrap (except for useful dropdowns)

---
+++ bootstrap1/js/stickySideNav.js
@@ -0,0 +1,15 @@
+// function setNavbar() { 
+//     if ($(document).scrollTop() > 390) { 
+// 	$('.side-nav').addClass('sticky'); 
+//     } else { 
+// 	$('.side-nav').removeClass('sticky'); 
+//     } 
+// } 
+// $(document).ready(function(){ 
+//     setNavbar(); 
+    
+//     $(window).scroll(function() { 
+// 	setNavbar(); 
+//     }); 
+    
+// }); 
